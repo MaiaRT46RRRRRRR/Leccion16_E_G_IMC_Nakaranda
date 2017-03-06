ba748918781fc08c5ef9a1bf4da01b7294251db4
nakarid <hamdal@gmail.com>
2017-03-05 21:24:24 -0500

    final final

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,6 @@ var calcular = document.getElementById('calcular');
 
 calcular.onclick = function() {
   var texto = /^[a-zA-Z]*$/;
-  var numeros = /^[0-9]*$/;
   var nombre = document.getElementById('nombre').value ;
   var peso= document.getElementById('peso').value ;
   var estatura = document.getElementById('estatura').value ;
@@ -10,9 +9,7 @@ calcular.onclick = function() {
 
 if (nombre !="" && peso != "" && estatura!="") {
    if(nombre.search(texto)) {
-         alert("Ingresa sólo letras");}
-        else if ((peso.search(numeros))||(estatura.search(numeros))){
-         alert("Ingresa sólo numero en los valores de peso y estatura");}
+         alert("Ingresa sólo letras en el nombre");}
          else {
          var x= "<li> Hola  " + nombre.toUpperCase() +
                 " ,tu Indice de Masa Corporal es : " +
